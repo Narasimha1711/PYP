2c9b8cedfc673fae8aea994b7c46b25a326204b2
KarthikeyaGolla <gskkarthikeya@gmail.com>
2025-03-15 20:27:54 +0530

    user model

---
+++ Backend/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    rollNo: { type: String, required: true },
+    name: { type: String, required: true },
+    mobileNumber: { type: Number, required: true },
+    skills: {
+        type: [String],
+        enum: ["Web Development", "Machine Learning", "Cyber Security", "DSA"],
+        required: false
+    },
+    projectsAchievements: { type: [String], required: false },
+    interestedSubjects: { type: [String], required: false },
+    is_private: { type: Boolean, required: false }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
