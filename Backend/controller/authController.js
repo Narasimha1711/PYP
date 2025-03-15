b694a395760b66fa88c49b807aa39e4fd6fcb7b6
KarthikeyaGolla <gskkarthikeya@gmail.com>
2025-03-15 23:55:20 +0530

    controller codes Written for authController

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -3,24 +3,25 @@ const User = require('../models/User');
 
 const signup = async (req, res) => {
     try {
-        const { rollNo, name, gmail, password, mobileNumber, skills, projectsAchievements, interestedSubjects, is_private } = req.body;
+        const { rollNo, name, gmail, password, mobileNumber, languages, skills, projectsAchievements, interestedSubjects, is_private } = req.body;
 
-        
+        // Check if user already exists
         const existingUser = await User.findOne({ gmail });
         if (existingUser) {
             return res.status(400).json({ message: 'User already exists' });
         }
 
-        
+        // Hash password
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        
+        // Create new user
         const newUser = new User({
             rollNo,
             name,
             gmail,
             password: hashedPassword,
             mobileNumber,
+            languages,
             skills,
             projectsAchievements,
             interestedSubjects,
