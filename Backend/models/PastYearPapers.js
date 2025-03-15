99bd437fbea670c9ee98ae5b16e9f6ff1eb97fb7
KarthikeyaGolla <gskkarthikeya@gmail.com>
2025-03-15 20:25:47 +0530

    user model

---
+++ Backend/models/PastYearPapers.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const ExamSchema = new mongoose.Schema({
+    year: { type: String, required: false },
+    papers: { type: [String], required: false }
+});
+
+const PastYearPapersSchema = new mongoose.Schema({
+    subjectId: { type: String, required: false },
+    subject: { type: String, required: false },
+    mid1: { type: ExamSchema, required: false },
+    mid2: { type: ExamSchema, required: false },
+    end: { type: ExamSchema, required: false }
+});
+
+const PastYearPapers = mongoose.model('PastYearPapers', PastYearPapersSchema);
+
+module.exports = PastYearPapers;
